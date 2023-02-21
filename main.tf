59c658964036da3f5b1c101b8d6c2b64c728d5e9
txrr6 <pdemelexi@gmail.com>
2023-02-20 23:02:32 -0800

    added main.tf

---
+++ main.tf
@@ -0,0 +1,28 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+
+resource "aws_s3_bucket" "b" {
+  bucket = "my-tf-test-bucket-sentinel"
+
+  #tags = {
+  #  Name        = "My bucket"
+  #  Environment = "Dev"
+  #}
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = aws_kms_key.mykey.arn
+        sse_algorithm     = "aws:kms"
+      }
+    }
+  }
+
+}
+
+resource "aws_kms_key" "mykey" {
+  description             = "This key is used to encrypt bucket objects"
+  deletion_window_in_days = 10
+}
